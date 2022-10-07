ff6956e3c70d9ac4b26ecc7f27a6c8062a712b4d
Daria-Lytvynenko <83474527+Daria-Lytvynenko@users.noreply.github.com>
2022-10-07 16:27:01 +0300

    task 3

--- sum_in_array.py
+++ sum_in_array.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 21 12:59:22 2022
-
-@author: Okhrimchuk Roman & Maksym Veremchuk
-for Sierentz Global Merchants
-
-
 Test task
 """
+def lucky_series(numbers):
+    combination=re.split('[0-4, 7-9]', numbers)
+    combination=[i for i in combination if len(set(i))!=1 and len(set(i))!=0]
+    try:
+        result=max(combination, key=len)
+        return result
+    except ValueError:
+        return 0
