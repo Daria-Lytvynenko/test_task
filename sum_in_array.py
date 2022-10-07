06e467c70e016a0be54f3c430b2df01fa2ef0f3a
Daria-Lytvynenko <83474527+Daria-Lytvynenko@users.noreply.github.com>
2022-10-07 16:42:49 +0300

    Update sum_in_array.py

--- sum_in_array.py
+++ sum_in_array.py
@@ -1,10 +1,11 @@
+"""
 Test task
 """
-def lucky_series(numbers):
-    combination=re.split('[0-4, 7-9]', numbers)
-    combination=[i for i in combination if len(set(i))!=1 and len(set(i))!=0]
-    try:
-        result=max(combination, key=len)
-        return result
-    except ValueError:
-        return 0
+def summ_in_array(array, s):
+    pairs=[-1]
+    for num1 in array:
+        for i in range(array.index(num1), len(array) - 1):
+            num2=array[i+1]
+            if num1 + num2 == s:
+                pairs.append([num1, num2])
+    return pairs
