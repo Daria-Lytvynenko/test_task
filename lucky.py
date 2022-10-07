52dd05fcfa3333e0821aa41c9df222cdd102a552
Daria-Lytvynenko <83474527+Daria-Lytvynenko@users.noreply.github.com>
2022-10-07 16:45:02 +0300

    Update lucky.py

--- lucky.py
+++ lucky.py
@@ -1,10 +1,12 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Mar 21 12:59:51 2022
-
-@author: Okhrimchuk Roman & Maksym Veremchuk
-for Sierentz Global Merchants
-
-
 Test task
 """
+
+def lucky (numbers):
+    combination = re.split('[0-4, 7-9]', str(numbers))
+    combination = [i for i in combination if len(set(i)) != 1 and len(set(i)) != 0]
+    try:
+        result = max(combination, key=len)
+        return result
+    except ValueError:
+        return 0
